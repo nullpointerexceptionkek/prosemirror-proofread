5cbccbe58744dbeca55bcbb23da7cfff340e7cb7
Louis Li <lee178lee@gmail.com>
2024-12-28 20:28:19 -0800

    build: Update build process to include demo and CSS bundling

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  entry: ['src/lib/index.ts'],
-
-  outDir: 'dist',
-
-  format: ['esm', 'cjs'],
-
-  dts: true,
-
-  clean: true,
-});
+export default defineConfig([
+	{
+		entry: ['src/lib/index.ts'],
+		outDir: 'dist',
+		format: ['esm', 'cjs'],
+		dts: true,
+		clean: true,
+		bundle: true
+	},
+	{
+		entry: ['src/lib/suggestion.css'],
+		outDir: 'dist'
+	}
+]);
